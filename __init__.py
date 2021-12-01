8f3e936d1da37365dd810f568b7b075c68ad2f8e
OTRAS TITULACIONES <0468937@lab.it.uc3m.es>
2021-12-01 16:28:32 +0100

    fix

--- __init__.py
+++ __init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = ""
+    app.config["SQLALCHEMY_DATABASE_URI"] = ""
+
+
+
     from . import main
     app.register_blueprint(main.bp)
 
--- auth.py
+++ auth.py
@@ -1,10 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from . import db, bcrypt
+
+from . import model
 
 bp = Blueprint("auth", __name__)
 
 @bp.route("/signup")
 def signup():
     return render_template("auth/signup.html")
+
+@bp.route("/signup", methods=["POST"])
+def signup_post():
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if password != request.form.get("password_repeat"):
+        return redirect(url_for("auth.signup"))
+
+    user = model.User.query.filter_by(email=email).first()
+    if user:
+        return redirect(url_for("auth.signup"))
+
+    password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+    new_user = model.User(email=email, name=username, password=password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for("main.index"))
     
 @bp.route("/login")
 def login():
